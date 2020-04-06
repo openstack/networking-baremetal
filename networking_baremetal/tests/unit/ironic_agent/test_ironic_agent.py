63da30ccccde03d127eef0eb2f2460aaf21e9620
Riccardo Pittau <elfosardo@gmail.com>
2020-04-06 09:28:06 +0200

    Use mock from unittest
    Use mock from unittest standard library.
    
    Change-Id: Iad08fb2a508927689ca907920491050b3fbb1a3b

--- networking_baremetal/tests/unit/ironic_agent/test_ironic_agent.py
+++ networking_baremetal/tests/unit/ironic_agent/test_ironic_agent.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import mock
+from unittest import mock
 
 from ironicclient import client
 import ironicclient.common.apiclient.exceptions as ironic_exc
